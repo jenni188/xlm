9b43c5717fadc122fc640f2b66966e6600f666ad
Jenni <jennihynynen80@gmail.com>
2021-08-13 11:47:47 +0300

    toka commit

---
+++ parser.js
@@ -0,0 +1,18 @@
+window.addEventListener("load", loadXML);
+
+
+
+function loadXML(){
+    let ajax = new XMLHttpRequest();
+    ajax.onload = function(){
+        parseXML(this.responseXML);
+    }
+    ajax.open("GET", "data.xml", true);
+    ajax.send();
+}
+
+function parseXML(xml){
+    const elokuva = xml.getElementsByTagName("elokuva");
+    console.log(elokuva);
+}
+
