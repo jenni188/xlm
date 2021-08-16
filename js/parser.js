21eac94e5f12d88d20fb2a84ba116c060e3fcec7
Jenni <jennihynynen80@gmail.com>
2021-08-16 10:53:04 +0300

    vika commit

--- js/parser.js
+++ js/parser.js
@@ -13,11 +13,15 @@ function loadXML(){
 
 function parseXML(xml){
     const elokuva = xml.getElementsByTagName("elokuva");
-    let htmlData = ";"
+    let htmlData = ""
 
     for(let i = 0; i < elokuva.length; i++){
-        htmlData += "<h2>" + elokuva[i].getElementsByTagName("genre")[0].childNodes[0].nodeValue + "</h2>";
-        htmlData += "<p>" + elokuva[i].getElementsByTagName("nimi")[0].childNodes[0].nodeValue + "</p>";
+        htmlData += "<h2>" + elokuva[i].getElementsByTagName("nimi")[0].childNodes[0].nodeValue + "</h2>";
+        htmlData += "<p>" + elokuva[i].getElementsByTagName("ohjaaja")[0].childNodes[0].nodeValue + "</p>";
+        htmlData += "<p>" + elokuva[i].getElementsByTagName("vuosi")[0].childNodes[0].nodeValue + "</p>";
+        htmlData += "<p>" + elokuva[i].getElementsByTagName("genre")[0].childNodes[0].nodeValue + "</p>";
+        htmlData += "<p>" + elokuva[i].getElementsByTagName("ika")[0].childNodes[0].nodeValue + "</p>";
+        htmlData += "<p>" + elokuva[i].getElementsByTagName("yms")[0].childNodes[0].nodeValue + "</p>";
     }
     document.getElementById("elokuva").innerHTML = htmlData;
 }
--- xls/style.xsl
+++ xls/style.xsl
@@ -1,19 +1,22 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
-
     <xsl:template match="/">
         <html>
             <head>
-                <title>elokuva</title>    
+            <link rel="stylesheet" type="text/css" href="xsl.css"></link>
+                <title>Elokuva lista</title>    
             </head>
             <body>
-                <h1>elokuva</h1>
+                <h1>Elokuva lista</h1>
                 <hr/>
                 <xsl:for-each select="data/elokuva">
-                <div>
+                <div class="moi">
                     <h2><xsl:value-of select="nimi"/></h2>
-                    <h3><xsl:value-of select="vuosi"/></h3>
+                    <p><xsl:value-of select="ohjaaja"/></p>
+                    <p><xsl:value-of select="vuosi"/></p>
                     <p><xsl:value-of select="genre"/></p>
+                    <p><xsl:value-of select="ika"/></p>
+                    <p><xsl:value-of select="yms"/></p>
                 </div>
                 </xsl:for-each>
             </body>
